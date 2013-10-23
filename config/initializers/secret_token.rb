e1a6c57d013c3d9aca5e4d12e6508b3996c7650f
nab <napu1031@gmail.com>
2013-10-24 00:42:59 +0900

    add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Zenigame::Application.config.secret_token = '1c478662d14703a9e7d9996b6e96dc4c37fa48cbada2b1ee47453ec5e874e85efc740639480c20d86076b9d3f51e111c374fc882ffc450782098dc0ddbbdcf7b'
